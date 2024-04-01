f823040bec5305ad5c2de0107d329fdc488d3967
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-04-01 21:35:30 +0545

    user login/logout check

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,13 +1,26 @@
 
 import 'dart:async';
 import 'package:fire_base/ui/auth/login_screen.dart';
+import 'package:fire_base/ui/posts/post_screen.dart';
 import 'package:fire_base/ui/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 class SplashServies{
 
   void isLogin(BuildContext context){
 
-   Timer(Duration(seconds: 3), 
+    final auth = FirebaseAuth.instance;
+
+    final user = auth.currentUser;
+
+    if(user != null){
+    Timer(Duration(seconds: 3), 
+   ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> PostScreen())));
+  
+    }else{
+      Timer(Duration(seconds: 3), 
    ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen())));
+  
+    }
   }
 }
--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -1,4 +1,8 @@
+import 'package:fire_base/ui/auth/login_screen.dart';
+import 'package:fire_base/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -8,11 +12,28 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Post Screen"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                auth.signOut().then((value) {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                }).onError((error, stackTrace) {
+                  Utils().toastMessage(error.toString());
+                });
+              },
+              icon: Icon(Icons.logout_outlined)),
+          SizedBox(
+            width: 10,
+          )
+        ],
       ),
     );
   }

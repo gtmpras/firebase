9574bb9b9dc0372a49b34c9b9e4ed792bb4cc717
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-04-26 23:26:24 +0545

    realtime database

--- lib/ui/posts/add_post.dart
+++ lib/ui/posts/add_post.dart
@@ -1,3 +1,4 @@
+import 'package:fire_base/utils/utils.dart';
 import 'package:fire_base/widgets/round_button.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,10 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
-  final DatabaseRef = FirebaseDatabase.instance.ref('Post');
+  final postController = TextEditingController();
+  bool loading = false;
+  final DatabaseRef = FirebaseDatabase.instance
+      .ref('Post'); //creating table and providing table_name
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,22 +22,45 @@ class _AddPostScreenState extends State<AddPostScreen> {
         title: Text("Add Post"),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal:20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
-        SizedBox(height: 30,),
-        TextFormField(
-          maxLines: 4,
-          decoration: InputDecoration(
-            hintText: "What is in your mind? ",
-            border: OutlineInputBorder(),
-            
-          ),
-        ),
-        SizedBox(height: 30,),
-      RoundButton(title: "Add", ontap: (){
-        DatabaseRef.child(path)
-      })
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              controller: postController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: "What is in your mind? ",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            RoundButton(
+                title: "Add",
+                loading: loading,
+                ontap: () {
+                  setState(() {
+                    loading = true;
+                  });
+                  DatabaseRef.child('1').set({
+                    'title': postController.text.toString(),
+                    'id': 1,
+                  }).then((value) {
+                    Utils().toastMessage("Post Added");
+                    setState(() {
+                      loading = false;
+                    });
+                  }).onError((error, stackTrace) {
+                    Utils().toastMessage(error.toString());
+                    setState(() {
+                      loading= false;
+                    });
+                  });
+                })
           ],
         ),
       ),
